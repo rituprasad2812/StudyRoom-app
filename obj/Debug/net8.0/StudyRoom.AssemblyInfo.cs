998a35235fb4d9b846a4aacbe5f0bfdac295e2a2
unknown <rituprasad2812@gmail.com>
2025-10-30 23:03:55 +0530

    Add screenshots and update README

--- obj/Debug/net8.0/StudyRoom.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudyRoom.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyRoom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b7d18a07bbacbd589a661bc9c0847eddc8d83e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyRoom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyRoom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
